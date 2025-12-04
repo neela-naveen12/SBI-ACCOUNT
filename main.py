021023d3e0f26fd93fb73133ce17acc4c06882cb
Git-Demo <neelanaveen8868@gmail.com>
2025-12-04 12:00:24 +0530

    completed

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel
 
 from models import CreateAccountRequest, FundAccountRequest, WithdrawRequest
 from service import cerate_customer_account
+from service import fund_customer_account
+from service import fund_customer_withdraw
 
 app = FastAPI(title="SBI-POC-API")
 
@@ -24,9 +26,25 @@ async def cerate_account(data:CreateAccountRequest):
 
 @app.post("/fund-account")
 async def fund_amount(data:FundAccountRequest):
-    return True
+    result = await fund_customer_account(data.customer_id,data.amount)
+    if result:
+        return{
+            "messege": "fund added sucessfully!",
+        }
+    else:
+        return{
+            "messege":"we can,t"
+        }
 
 
 @app.post("/fund-withdraw")
 async def fund_withdraw(data:WithdrawRequest):
-    return True
+    result = await fund_customer_withdraw(data.customer_id,data.amount)
+    if result:
+        return{
+            "messege":"sucessfully withdraw amount{customer_id}"
+        }
+    else: 
+        return{
+            "messege":"we cant withdraw amount!"
+        }
--- service.py
+++ service.py
@@ -7,3 +7,25 @@ async def cerate_customer_account(customer_id:str):
         return False
     accounts[customer_id] = 0.0
     return True
+
+
+async def fund_customer_account( customer_id:str,amount:float):
+    if customer_id not in accounts:
+        raise ValueError(f"account does not exist for customer with id {customer_id}")
+    
+    accounts[customer_id] += amount
+    return accounts[customer_id]
+
+async def fund_customer_withdraw(customer_id: str, amount: float):
+    if customer_id in accounts:
+        accounts[customer_id] -= amount
+        return {
+            "message": "Withdraw successful",
+            "customer_id": customer_id,
+            "remaining_balance": accounts[customer_id]
+        }
+    
+    raise ValueError(f"There is no account with id {customer_id}")
+
+
+    
